f3d952dc687362b3bfef87dcb54e8116a4cc7c53
Bilalhassan679 <ftk4321@gmail.com>
2022-01-14 22:22:03 +0500

    Rendering API data.ASif Taj tutorial

---
+++ android/app/src/main/kotlin/com/example/apiasiftaj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apiasiftaj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:apiasiftaj/Models/posts_model.dart';
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PostsModel> postList=[];
+
+  Future<List<PostsModel>?> getPostApi() async
+  {
+    final response=await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data=jsonDecode(response.body.toString());
+    if(response.statusCode==200){
+        for(Map i in data){
+          postList.add(PostsModel.fromJson(i));
+        }
+        return postList;
+    }
+    else  {
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Api'),),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getPostApi(),
+                builder: (context, snapshot)
+                {
+                  if(!snapshot.hasData){
+                    return Text('Loading');
+                  }
+                  else
+                  return ListView.builder(
+                      itemCount: postList.length,
+                      itemBuilder: (context,index){
+                    return Text(index.toString());
+                  });
+            }),
+          )
+        ],
+      ),
+    );
+  }
+}
